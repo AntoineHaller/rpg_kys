1481608403430f7014ca1c8fe09c3f71b6ca03ec
EmmanuelEskander07 <34535442+EmmanuelEskander07@users.noreply.github.com>
2018-12-14 10:33:18 +0100

    Update Menu.cs

--- Rpg_Kys/Rpg_Kys/Menu.cs
+++ Rpg_Kys/Rpg_Kys/Menu.cs
@@ -11,19 +11,19 @@ namespace Rpg_Kys
         public Menu()
         {
             PrintMenu();
-            int choix = AskChoice(0, 3);
+            int choix = AskChoice(1, 3);
             switch (choix)
             {
-                case 0:
+                case 1:
                     StartGame();
                     break;
-                case 1:
+                case 2:
                     Load();
                     break;
-                case 2:
+                case 3:
                     Apropos();
                     break;
-                case 3:
+                case 4:
                     Quit();
                     break;
             }
@@ -31,7 +31,28 @@ namespace Rpg_Kys
 
         public void PrintMenu()
         {
-            //TODO Faire le PrintMenu
+            Console.WriteLine("\n\n");
+            Console.WriteLine("                      /$$   /$$       /$$     /$$        /$$$$$$ ");
+            Console.WriteLine("                     | $$  /$$/      |  $$   /$$/       /$$__  $$");
+            Console.WriteLine("                     | $$ /$$/        \\  $$ /$$/       | $$  \\__/");
+            Console.WriteLine("                     | $$$$$/          \\  $$$$/        |  $$$$$$ ");
+            Console.WriteLine("                     | $$  $$           \\  $$/          \\____  $$");
+            Console.WriteLine("                     | $$\\  $$           | $$           /$$  \\ $$");
+            Console.WriteLine("                     | $$ \\  $$          | $$          |  $$$$$$/");
+            Console.WriteLine("                     |__/  \\__/          |__/           \\______/ " + "\n\n\n");
+
+
+            Console.WriteLine("Bienvenue dans le monde merveilleux de KYS\n");
+            Console.WriteLine("La vie en entreprise n'est pas toujours radieuse. Heureusement vous avez trouvé la solution à votre malheur.");
+            Console.WriteLine("Votre but est d'atteindre le dernier étage de la tour dans laquelle vous travaillez.");
+            Console.WriteLine("Cependant vous devez faire attention, de nombreux obstacles vous barreront la route.\n");
+            
+            Console.WriteLine("1 - Nouvelle partie");
+            Console.WriteLine("2 - Charger une partie");
+            Console.WriteLine("3 - A propos");
+            Console.WriteLine("4 - Quitter");
+            
+            
         }
 
         public static int AskChoice(int min, int max)
