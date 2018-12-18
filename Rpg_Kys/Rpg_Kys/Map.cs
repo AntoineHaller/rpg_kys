23cdc760089e3e5fa5fd93831f426ab36534142b
EmmanuelEskander07 <34535442+EmmanuelEskander07@users.noreply.github.com>
2018-12-18 09:29:42 +0100

    Add files via upload

--- Rpg_Kys/Rpg_Kys/Map.cs
+++ Rpg_Kys/Rpg_Kys/Map.cs
@@ -8,18 +8,32 @@ namespace Rpg_Kys
 {
     class Map
     {
-        public static int hauteur = 5;
-        public static int largeur = 3;
+        public static int hauteur = 4;
+        public static int largeur = 2;
         public Case[,] La_map = new Case[largeur, hauteur];
 
         public Map()
         {
-            for (int i = 0; i < largeur; i++)
+            for (int j = 0; j < hauteur; j++)
             {
-                for (int j = 0; j < hauteur; j++)
+                for (int i = 0; i < largeur; i++)
                 {
-                    Random r = new Random(DateTime.Now.Millisecond);
-                    La_map[i, j] = new Case(); //TODO random case
+                    if(j == 0)
+                        La_map[i, j] = new Case(Case.CaseType.Rien, "Vous êtes en bas de votre immeuble, rien ne se passe.");
+                    if(j == 1)
+                        La_map[i, j] = new Case(Case.CaseType.Rencontre, "Vous croisez un de vos collegues, il ne semble pas vouloir vous laisser passer.");
+                    if (j == 2)
+                        La_map[i, j] = new Case(Case.CaseType.Loot, "En fouillant bien autour de vous, vous trouvez un objet intéressant.");
+                    if (j == 3 && i ==1)
+                        La_map[i, j] = new Case(Case.CaseType.Enigme, "Un post-it vous attend. Quelques lignes sont inscrites dessus. Vous vous sentez obligé d'y répondre.");
+                    if (j == 3)
+                        La_map[i, j] = new Case(Case.CaseType.Fotocopieuse, "Vous tombez nez à nez à une FOTOcopieuse. Pas le choix. Elle sent votre peur. Vous devez attendre qu'elle se rendorme.");
+                    if (j == 4 && i == 2)
+                        La_map[i, j] = new Case(Case.CaseType.Pdg, "Vous y êtes enfin. L'antre du boss final est devant vous.");
+                    if (j == 4)
+                        La_map[i, j] = new Case(Case.CaseType.Rencontre, "Vous croisez un de vos collegues, il ne semble pas vouloir vous laisser passer.");
+
+
                 }
             }
         }
