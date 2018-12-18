87678f602750408de66ed0423c3f8d6b25eaf854
AntoineHaller <34535243+AntoineHaller@users.noreply.github.com>
2018-12-18 10:36:46 +0100

    Add files via upload

--- Rpg_Kys/Rpg_Kys/Weapon.cs
+++ Rpg_Kys/Rpg_Kys/Weapon.cs
@@ -10,15 +10,13 @@ namespace Rpg_Kys
     {
         public enum WeaponType { Balai, Clavier, Missile_Nucleaire, Lettre_de_licenciement, bloc_note, Telephone, calculatrice, Carte_acces, Extincteur, Bouquet_de_fleurs }; //TODO mettre les bonnes armes
         public WeaponType Type;
-        public int Rarity; //en liaison avec des couleurs ?
         public int Plus_de_degat;
         public int Plus_de_charisme;
         public int Plus_d_intelligence;
 
-        public Weapon(string name, string description, int weight, WeaponType t, int rarity, int plus_de_degat, int plus_de_charisme, int plus_d_intelligence) : base(name, description, weight)
+        public Weapon(string name, string description, int weight, WeaponType t, int plus_de_degat, int plus_de_charisme, int plus_d_intelligence) : base(name, description, weight)
         {
             Type = t;
-            Rarity = rarity;
             Plus_de_degat = plus_de_degat;
             Plus_de_charisme = plus_de_charisme;
             Plus_d_intelligence = plus_d_intelligence;
