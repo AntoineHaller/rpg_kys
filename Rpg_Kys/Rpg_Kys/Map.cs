cbb59a18310f44f363909c05161905e571b4c642
AntoineHaller <34535243+AntoineHaller@users.noreply.github.com>
2018-12-19 10:04:03 +0100

    Add files via upload

--- Rpg_Kys/Rpg_Kys/Map.cs
+++ Rpg_Kys/Rpg_Kys/Map.cs
@@ -8,46 +8,56 @@ namespace Rpg_Kys
 {
     class Map
     {
-        public static int hauteur = 4;
-        public static int largeur = 2;
-        public Case[,] La_map = new Case[largeur, hauteur];
+        public static int pos_x = 1;
+        public static int pos_y = 0;
+        public static int x = 4;
+        public static int y = 2;
+        public static Case[,] La_map = new Case[x, y];
 
         public Map()
         {
-            for (int j = 0; j < hauteur; j++)
-            {
-                for (int i = 0; i < largeur; i++)
-                {
-                    if(j == 0)
-                        La_map[i, j] = new Case(Case.CaseType.Rien, "Vous êtes en bas de votre immeuble, rien ne se passe.");
-                    if(j == 1)
-                        La_map[i, j] = new Case(Case.CaseType.Rencontre, "Vous croisez un de vos collegues, il ne semble pas vouloir vous laisser passer.");
-                    if (j == 2)
-                        La_map[i, j] = new Case(Case.CaseType.Loot, "En fouillant bien autour de vous, vous trouvez un objet intéressant.");
-                    if (j == 3 && i ==1)
-                        La_map[i, j] = new Case(Case.CaseType.Enigme, "Un post-it vous attend. Quelques lignes sont inscrites dessus. Vous vous sentez obligé d'y répondre.");
-                    if (j == 3 && i !=1)
-                        La_map[i, j] = new Case(Case.CaseType.Fotocopieuse, "Vous tombez nez à nez à une FOTOcopieuse. Pas le choix. Elle sent votre peur. Vous devez attendre qu'elle se rendorme.");
-                    if (j == 4 && i == 2)
-                        La_map[i, j] = new Case(Case.CaseType.Pdg, "Vous y êtes enfin. L'antre du boss final est devant vous.");
-                    if (j == 4 && i != 2)
-                        La_map[i, j] = new Case(Case.CaseType.Rencontre, "Vous croisez un de vos collegues, il ne semble pas vouloir vous laisser passer.");
-
-
-                }
-            }
         }
 
-        public void Print_Map()
+        public static void position_joueur()
         {
-            for (int i = 0; i < largeur; i++)
+
+        }
+
+        public static void test_case_joueur(int j, int i)
+        {
+            if (j == 0 && i == 0)
+                La_map[i, j] = new Case(Case.CaseType.Rien, "Vous êtes en bas de votre immeuble, rien ne se passe.");
+            if (j == 1 && i == 0)
+                La_map[i, j] = new Case(Case.CaseType.Rencontre, "Vous croisez un de vos collegues, il ne semble pas vouloir vous laisser passer.");
+            if (j == 2 && i == 0)
+                La_map[i, j] = new Case(Case.CaseType.Loot, "En fouillant bien autour de vous, vous trouvez un objet intéressant.");
+            if (j == 3 && i == 0)
+                La_map[i, j] = new Case(Case.CaseType.Enigme, "Un post-it vous attend. Quelques lignes sont inscrites dessus. Vous vous sentez obligé d'y répondre.");
+            if (j == 0 && i == 1)
+                La_map[i, j] = new Case(Case.CaseType.Fotocopieuse, "Vous tombez nez à nez à une FOTOcopieuse. Pas le choix. Elle sent votre peur. Vous devez attendre qu'elle se rendorme.");
+            if (j == 1 && i == 1)
+                La_map[i, j] = new Case(Case.CaseType.Pdg, "Vous y êtes enfin. L'antre du boss final est devant vous.");
+            if (j == 2 && i == 1)
+                La_map[i, j] = new Case(Case.CaseType.Rencontre, "Vous croisez un de vos collegues, il ne semble pas vouloir vous laisser passer.");
+            if (j == 3 && i == 1)
+                La_map[i, j] = new Case(Case.CaseType.Rencontre, "Vous croisez un de vos collegues, il ne semble pas vouloir vous laisser passer.");
+        }
+
+
+        public static void Print_Map()
+        {
+            Console.WriteLine("\n Voici la map, à vous de vous déplacer");
+
+            for (int i = 0; i < y; i++)
             {
-                for (int e = 0; e < hauteur; e++)
+                for (int e = 0; e < x; e++)
                 {
                     Console.Write("[" + "  " + "]");
                 }
                 Console.WriteLine();
             }
+
+            Map.test_case_joueur(pos_x, pos_y);
         }
 
         public string GetDescription(int x, int y) //retourne la description de la case
